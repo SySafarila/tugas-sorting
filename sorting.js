0e432268642469f934706c35fbf7f30387430665
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2021-06-01 15:21:50 +0700

    Add comments

--- sorting.js
+++ sorting.js
@@ -4,15 +4,18 @@ let fruits = ["Semangka", "Apel", "Mangga", "Pisang", "Anggur", "Sirsak"];
 let fruitsMap = fruits.map((data) => {
   return `<li>${data}</li>`;
 });
-document.getElementById("fruits").innerHTML = fruitsMap.join("\n");
+document.getElementById("fruits").innerHTML = fruitsMap.join("\n"); // Menampilkan data
 
 // Dengan sorting
 const fruitsSorting = () => {
-  let sort = fruits.sort();
+  let sort = fruits.sort(); // Sorting (Mengurutkan) data
+
+  // Memisahkan buah-buah
   let result = sort.map((fruit) => {
     return `<li>${fruit}</li>`;
   });
 
+  // Menampilkan data
   return (document.getElementById("fruitsSorting").innerHTML =
     result.join("\n"));
 };
